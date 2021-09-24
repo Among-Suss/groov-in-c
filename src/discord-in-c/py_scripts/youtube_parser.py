26b8b6e96653f8bb7b317cc5abd35ebb9b87d8ae
poohcom1 <pooh677@gmail.com>
2021-09-24 18:49:57 +0700

    Improved playlist retry system

--- src/discord-in-c/py_scripts/youtube_parser.py
+++ src/discord-in-c/py_scripts/youtube_parser.py
@@ -4,10 +4,14 @@ import json
 import sys
 import os
 import time
+import traceback
 from datetime import datetime
 import argparse
 
 RETRIES = 10
+
+class EndRetry(Exception): pass
+class AttemptRetry(Exception): pass
 
 def get_description(text):
     """Retrieves the video description
@@ -43,7 +47,10 @@ def get_playlist_data(text, do_save=False):
     if playlist_mode:
         content_list = parsed_json['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['playlistVideoListRenderer']['contents']
     else:
-        content_list = parsed_json['contents']['twoColumnWatchNextResults']['playlist']['playlist']['contents']
+        try:
+            content_list = parsed_json['contents']['twoColumnWatchNextResults']['playlist']['playlist']['contents']
+        except KeyError as e:
+            raise EndRetry("Playlist key error") from e
 
     # Filter out missing videos
     content_list = [x for x in content_list if "unplayableText" not in x]
@@ -55,7 +62,10 @@ def get_playlist_data(text, do_save=False):
             continue
         video = data['playlistVideoRenderer' if playlist_mode else 'playlistPanelVideoRenderer']
 
-        duration = video["lengthText"]["simpleText"]
+        try:
+            duration = video["lengthText"]["simpleText"]
+        except KeyError as e:
+            raise AttemptRetry("Length key error") from e
         length_vals = duration.split(":")
 
         if len(length_vals) > 2:
@@ -84,11 +94,14 @@ def retry(retries, func, *args) -> Any:
     for _ in range(retries + 1):
         try:
             return func(*args)
-        except Exception as e:
-            log(e)
+        except AttemptRetry:
+            log(traceback.format_exc())
             time.sleep(backoff)
             backoff += 1
-            pass
+        except EndRetry:
+            log(traceback.format_exc())
+            break
+            
     sys.exit(1)
 
 # Utils
@@ -125,17 +138,21 @@ if __name__ == "__main__":
     if debug:
         print("====DEBUG MODE====")
 
-
     def fetch_and_parse():
-        text = requests.get(url).text
+        try:
+            text = requests.get(url).text
+        except Exception as e:
+            raise AttemptRetry("Fetch error") from e
 
         if debug:
             with open('__pycache__/test.html', 'w') as fp:
                 fp.write(text)
-
+        
         if command == 'description':
             print(get_description(text))
         elif command == 'playlist':
-            print(json.dumps(get_playlist_data(text, debug), indent=2 if debug else None))
+            print(json.dumps(get_playlist_data(
+                text, debug), indent=2 if debug else None))
+
 
     retry(retries, fetch_and_parse)
