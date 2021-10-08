316a39521424d504472acd05f228a42e78f64af8
TanapTheTimid <tanapoom.se@gmail.com>
2021-10-09 01:42:57 +0700

    added back -g compiler flag for more detail from sanitizer when crash. Fix potential crash due to race condition cause youtube object pointer to be null from sbuf peek end value in threaded play

--- src/discord-in-c/media.c
+++ src/discord-in-c/media.c
@@ -635,6 +635,11 @@ void *media_player_threaded(void *ptr) {
   while (sem_trywait(&(yptr->media_player_t_ptr->quitter)) < 0) {
     ytpobj_p = sbuf_peek_end_value_direct(
         &(yptr->media_player_t_ptr->song_queue), NULL, 1);
+    
+    if(!ytpobj_p){
+      sbuf_stop_peeking(&(yptr->media_player_t_ptr->song_queue));
+      continue;
+    }
 
     if (sem_trywait(&(yptr->media_player_t_ptr->quitter)) >= 0) {
       break;
